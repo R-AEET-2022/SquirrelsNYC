43cac18e3643bf45bcb56c55574e45ed9cf104e9
Irene Repeto <irene.repeto@uca.es>
2022-09-20 23:44:28 +0200

    que le pasa

--- squirrel_data.R
+++ squirrel_data.R
@@ -1,7 +1,6 @@
-
-library(here) #permite hacer referencia siempre al directorio donde se encuentra el proyecto
-
+library(here) 
 library(readr)
+
 data <- read_csv("2018_Squirrel_Data.csv")
 
 glimpse(data)
@@ -22,3 +21,15 @@ data_sum <- data %>% select(X,
                             16,17,18,19,20,27,28,29)
 
 dfSummary(data_sum)
+
+#create var "activity" - what was the sq doing at moment of obs.?
+data_sum2 <- data_sum %>% 
+  mutate(activity = case_when (Running == 'TRUE' ~ 'running',
+                               Chasing == "TRUE" ~ "chasing",
+                               Climbing == "TRUE" ~ "climbing",
+                               Eating == "TRUE" ~ "eating",
+                               Foraging == "TRUE" ~ "foraging"))
+
+glimpse(data_sum2)
+data_sum2$activity<- as.factor(data_sum2$activity)
+
