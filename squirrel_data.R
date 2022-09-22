227e0cda7110014e44e7521f8e3aa031561b8914
AlvaroPG9 <alvaro.perez@gm.uca.es>
2022-09-22 13:09:02 +0200

    intento

--- squirrel_data.R
+++ squirrel_data.R
@@ -1,5 +1,6 @@
 library(here) 
 library(readr)
+library(dplyr)
 
 data <- read_csv("2018_Squirrel_Data.csv")
 
@@ -14,7 +15,7 @@ library(summarytools) # RESUMEN DE LAS VARIABLES (niveles, freq, graph, missing 
 #create a subset with (possibly) interesting variables
 data_sum <- data %>% select(X, 
                             Y,
-                            `Unique Squirrel ID`,
+                            `Unique_Squirrel_ID`,
                             Shift,
                             Date,
                             Age, 
@@ -22,6 +23,9 @@ data_sum <- data %>% select(X,
                             16,17,18,19,20,27,28,29)
 
 dfSummary(data_sum)
+
+#talnn
+#kkkk
 
 #create var "activity" - what was the sq doing at moment of obs.?
 
@@ -34,3 +38,66 @@ data_sum2 <- data_sum %>%
                                Foraging == "TRUE" ~ "foraging")) 
   
 data_sum2$activity<- as.factor(data_sum2$activity)
+
+
+data_sum3<-na.omit(data_sum)
+data_sum3<-data_sum3[!(data_sum3$Age=="?"),]
+
+data_sum=NULL
+data_sum2=NULL
+
+plot(factor(Running) ~ factor(Age), data = data_sum3)
+
+
+#¿qué necesitan las ardillas para comer?
+#varios modelos ---------
+sq <- glm(Eating ~ Shift+Location,
+          data = data_sum3,
+          family = binomial)
+summary(sq)
+library(report)
+report(sq)
+
+sq4 <- glm(Eating ~ Shift,
+          data = data_sum3,
+          family = binomial)
+summary(sq4)
+
+library(report)
+report(sq4)
+
+sq1 <- glm(Eating ~ Location,
+          data = data_sum3,
+          family = binomial)
+summary(sq1)
+
+sq2 <- glm(Eating ~ Location*Age,
+           data = data_sum3,
+           family = binomial)
+summary(sq2)
+
+
+sq3 <- glm(Eating ~ Location*Shift,
+          data = data_sum3,
+          family = binomial)
+summary(sq3)
+
+AIC(sq, sq1, sq2, sq3, sq4)
+
+
+library("effects")
+allEffects(sq)
+plot(allEffects(sq))
+
+library(visreg)
+visreg(sq, scale = "response", rug = FALSE)
+
+#Es decir, ya sea tarde o temprano, las ardillas suelen estar
+#comiendo en el suelo
+
+#Model checking ----------
+
+library("DHARMa")
+simulateResiduals(sq, plot = TRUE)
+library(performance)
+check_predictions(sq)
